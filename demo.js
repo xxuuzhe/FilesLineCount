26747bb9a974d8037c894e8073effba8aa10efa8
xxuuzhe <394758764@qq.com>
2015-06-01 20:58:46 +0800

    Count total files lines from David

---
+++ demo.js
@@ -0,0 +1,32 @@
+var fs = require('fs')
+var path = require('path')
+var filesCount = [];
+var filters = ['.js','.html','.css','.tpl']
+var num = 0;
+count("./")
+
+function count(pathWay){
+    var files = fs.readdirSync(pathWay);
+    files.forEach(function(name){
+        var tmpPath = pathWay +"/"+name
+        var tmpFile = fs.statSync(tmpPath)
+        if(tmpFile.isDirectory()){
+            count(tmpPath)
+        }else{
+            if(filters.indexOf(path.extname(name)) >- 1 ){
+                filesCount.push(tmpPath)
+            }
+        }
+    })
+}
+
+filesCount.forEach(function(file){
+
+    var data = fs.readFileSync(file,'utf8')
+    var str = data.toString('utf8')
+    var res = str.match(/\r\n|\n\r|\r|\n/g)
+    if(res)num+=res.length;
+})
+
+console.log(num)
+
